6310605ccec4c6b53f231a6b2e95a4d8d1457f3d
JastiTarun <129149484+JastiTarun@users.noreply.github.com>
2026-02-26 17:06:22 -0500

    Create glue_etl_script.py

---
+++ glue/glue_etl_script.py
@@ -0,0 +1,9 @@
+# AWS Glue ETL Script
+
+"""
+This script reads raw review data from S3,
+performs cleaning and transformations,
+and writes processed output back to S3.
+
+Used in AWS Glue ETL Job.
+"""
